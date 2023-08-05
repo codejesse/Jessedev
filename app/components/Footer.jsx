1c03e156ad7aae61a30be62dd7591e1c9eb286c1
Jesse <jessebeke10@gmail.com>
2023-08-05 18:33:29 +0100

    Footer logo

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AcmeLogo } from './AcmeLogo'
 
 const Footer = () => {
     return (
@@ -8,8 +9,8 @@ const Footer = () => {
                     <div className="md:flex md:justify-between">
                         <div className="mb-6 md:mb-0">
                             <a href="https://flowbite.com/" className="flex items-center">
-                                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
-                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                                <AcmeLogo />
+                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">JESSE</span>
                             </a>
                         </div>
                         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
