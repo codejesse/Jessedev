2aae4ea1d8820ee1a68b1725d7bf4cd33a7aefcf
Jesse <jessebeke10@gmail.com>
2023-08-22 00:00:11 +0100

    project object file created

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -16,7 +16,7 @@ const Card = () => {
                     />
                 </div>
             </div>
-            <h1 className='font-normal text-[16px] mt-[20px]'>Webminds</h1>
+            <h1 className='font-normal text-[16px] mt-[20px]'>Venda landing</h1>
         </div>
     )
 }
