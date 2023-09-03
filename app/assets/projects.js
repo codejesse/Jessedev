a12b48c53815e618b561cab35299fafd39dff6dc
Jesse <jessebeke10@gmail.com>
2023-09-03 02:09:38 +0100

    project data file set-up

--- app/assets/projects.js
+++ app/assets/projects.js
@@ -1,14 +1,26 @@
 export const project = [
     {
-        "id": 1,
-        "title": "Venda landing",
-        "desc": "This is a simple description",
-        "img_url": "",
-        "links": [
+        id: 1,
+        title: "Venda landing",
+        desc: "This is a simple description",
+        img_url: "",
+        links: [
             {
                 "github": "",
                 "link": "",
             }
         ]
-    }
+    },
+    {
+        id: 2,
+        title: "Fleep Marketplace",
+        desc: "This is a simple description",
+        img_url: "",
+        links: [
+            {
+                "github": "",
+                "link": "",
+            }
+        ]
+    },
 ]
